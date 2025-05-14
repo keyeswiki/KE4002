5907c2a03846d54d8346fbd57be307fe10f22f1e
keyestudio <215650023@qq.com>
2025-05-14 13:34:21 +0800

    UPDATE

--- conf.py
+++ conf.py
@@ -65,7 +65,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+  "show_toc_level": 3,
+  "show_navbar_depth": 2,
+  "max_navbar_depth": 2,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
